96459ba0362165d8c2b2831cb59db1bfd247555e
Alexlee8888 <alexleeyt8888@gmail.com>
2025-01-25 15:12:01 -0500

    user model

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,11 @@
 import express from 'express';
+import { login, logout, signup } from '../controllers/auth.controller.js';
 const router = express.Router()
 
-router.get("/signup", (req, res) => {
-    res.send("signup route")
-})
+router.post("/signup", signup)
 
-router.get("/login", (req, res) => {
-    res.send("login route")
-})
+router.post("/login", login)
 
-router.get("/logout", (req, res) => {
-    res.send("logout route")
-})
+router.post("/logout", logout)
 
 export default router;
